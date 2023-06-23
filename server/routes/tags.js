0ac78b59f987f856d9afc536172efc0305818da4
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-23 20:25:07 +0100

    :sparkles: Add tags routes
    Functions include getting all tags for a user

---
+++ server/routes/tags.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const tagsRoutes = express.Router();
+const Database = require("./Database");
+const database = new Database();
+
+tagsRoutes.get("/get_list_of_tags_for_a_user", (req, res) => {
+        var userId = "1";
+
+        var query = "CALL get_all_tags_by_user_id(?);"
+        database.query(query,[userId], res);
+      });
+
+module.exports = tagsRoutes;
