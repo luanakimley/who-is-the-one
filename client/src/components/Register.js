2d12dc83e30ed34924913f5743dcd4a5496cdcd5
luanakimley <luanaa.kimley@gmail.com>
2023-06-23 13:07:30 +0100

    :sparkles: Develop basic register page with functionality

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { SERVER_HOST } from "../config/global_constants";
+import { Navigate } from "react-router-dom";
+
+import axios from "axios";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [navigate, setNavigate] = useState(false);
+
+  let registerUser = (e) => {
+    e.preventDefault();
+
+    let formData = new FormData();
+    formData.append("username", username);
+    formData.append("email", email);
+    formData.append("password", password);
+
+    axios.post(`${SERVER_HOST}/register_user`, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    setNavigate(true);
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div>
+      {navigate ? <Navigate to="/" /> : null}
       <h1>Register</h1>
+      <form>
+        <label>Username</label>
+        <input type="text" onChange={handleUsernameChange} />
+
+        <label>Email</label>
+        <input type="text" onChange={handleEmailChange} />
+
+        <label>Password</label>
+        <input type="text" onChange={handlePasswordChange} />
+
+        <button onClick={registerUser}>Register</button>
+      </form>
     </div>
   );
 }
