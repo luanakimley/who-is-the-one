b672a3a300cced4feb1975deb8e9232a773f0e50
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-24 04:26:50 +0100

    :sparkles: Add insert category route

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const Database = require("./Database");
 const database = new Database();
 
-router.get("/get_list_of_categories_by_user_id/:userId", (req, res) => {
+router.get("/categories/:userId", (req, res) => {
+        var userId = req.params.userId;
+        var query = "SELECT categories.category_id, categories.category_name FROM categories WHERE user_id = ?"
+        database.query(query, [userId], (result) => res.json(result));
+      });
+
+router.post("/insert_category", (req, res) => {
+        var categoryName = req.params.categoryName;
         var userId = req.params.userId;
 
-        var query = "SELECT categories.category_name FROM categories WHERE user_id = ?"
-
-        database.query(query, [userId], (result) => res.json(result));
+        var query = "INSERT INTO categories (category_name, user_id) VALUES (?);";
+        database.query(query, [userName, email, password], (result) =>res.send("Insert category with name ${categoryName}"));
       });
 
 module.exports = router;
