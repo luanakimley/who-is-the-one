a3037670c82693b5922909cedbcddac7257e9c15
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-24 03:00:00 +0100

    :card_file_box: Add users_categories_preferences table

--- server/db/db.sql
+++ server/db/db.sql
@@ -10,7 +10,7 @@ CREATE DATABASE IF NOT EXISTS voting_system DEFAULT CHARACTER SET utf8 COLLATE u
 USE voting_system;
 
 /*DROPS any of the tables that may already exists*/
-DROP TABLE IF EXISTS tags_in_candidates, tags, candidates_in_categories, categories, candidates, users;
+DROP TABLE IF EXISTS users_categories_preferences, tags_in_candidates, tags, candidates_in_categories, categories, candidates, users;
 
 /*CREATE users table*/
 CREATE TABLE users
@@ -70,6 +70,16 @@ CREATE TABLE tags_in_candidates
   UNIQUE (tag_id, candidate_id)
 );
 
+/*CREATE users_categories_preferences table*/
+CREATE TABLE users_categories_preferences
+(
+  category_id INT NOT NULL,
+  tag_id INT NOT NULL,
+  weight DOUBLE NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES categories(category_id),
+  FOREIGN KEY (tag_id) REFERENCES tags(tag_id),
+  UNIQUE (category_id, tag_id)
+);
 
 /*-----------------------------------------------------------------PROCEDURES-------------------------------------------------------------------------------------*/
 DROP PROCEDURE IF EXISTS get_all_tags_by_user_id;
@@ -84,5 +94,19 @@ BEGIN
   JOIN candidates_in_categories ON candidates.candidate_id = candidates_in_categories.candidate_id
   JOIN categories ON candidates_in_categories.category_id = categories.category_id
   WHERE categories.user_id = user_id;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS get_list_of_candidates_by_category;
+
+DELIMITER //
+CREATE PROCEDURE get_list_of_candidates_by_category(IN user_id VARCHAR(255), IN category_name VARCHAR(255))
+BEGIN
+  SELECT candidates.*, tags.tag_description FROM candidates
+  JOIN candidates_in_categories ON candidates.candidate_id = candidates_in_categories.candidate_id
+  JOIN categories ON candidates_in_categories.category_id = categories.category_id
+  JOIN tags_in_candidates ON candidates.candidate_id = tags_in_candidates.candidate_id
+  JOIN tags ON tags_in_candidates.tag_id = tags.tag_id
+  WHERE categories.user_id = user_id AND categories.category_name = category_name;
 END //
 DELIMITER ;
