f026a722e1164a3493c121b3a30fcce78f9799fa
luanakimley <luanaa.kimley@gmail.com>
2023-06-24 05:02:44 +0100

    :sparkles: Implement functionality for insert category

--- client/src/components/AddCategory.js
+++ client/src/components/AddCategory.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { SERVER_HOST } from "../config/global_constants";
 
 export default function AddCategory() {
   const [category, setCategory] = useState("");
+  const [cookies] = useCookies(["userId"]);
   const navigate = useNavigate();
 
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
   };
 
-  let addCategory = (e) => {
+  const addCategory = (e) => {
     e.preventDefault();
 
     let formData = new FormData();
-    formData.append("category", category);
+    formData.append("categoryName", category);
+    formData.append("userId", cookies.userId);
 
-    navigate("/add_candidates", { state: category });
+    axios
+      .post(`${SERVER_HOST}/insert_category`, formData, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then(() => {
+        navigate("/add_candidates", { state: category });
+      })
+      .catch((error) => {
+        console.error("Error adding category:", error);
+      });
   };
 
   return (
