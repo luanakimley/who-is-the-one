f50fd9a70f08d9d973b093d6fd2c15845e86eaa6
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-24 04:34:45 +0100

    :bug: Fix bug in insert category parameters

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -4,17 +4,17 @@ const Database = require("./Database");
 const database = new Database();
 
 router.get("/categories/:userId", (req, res) => {
-        var userId = req.params.userId;
-        var query = "SELECT categories.category_id, categories.category_name FROM categories WHERE user_id = ?"
+        let userId = req.params.userId;
+        let query = "SELECT categories.category_id, categories.category_name FROM categories WHERE user_id = ?"
         database.query(query, [userId], (result) => res.json(result));
       });
 
 router.post("/insert_category", (req, res) => {
-        var categoryName = req.params.categoryName;
-        var userId = req.params.userId;
+        let categoryName = req.params.categoryName;
+        let userId = req.params.userId;
 
-        var query = "INSERT INTO categories (category_name, user_id) VALUES (?);";
-        database.query(query, [userName, email, password], (result) =>res.send("Insert category with name ${categoryName}"));
+        let query = "INSERT INTO categories (category_name, user_id) VALUES (?, ?);";
+        database.query(query, [categoryName, userId], (result) =>res.send('Insert category with name ${categoryName}'));
       });
 
 module.exports = router;
