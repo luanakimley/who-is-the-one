8424abdd627c88b372d40577096fe74568414f75
luanakimley <luanaa.kimley@gmail.com>
2023-06-25 15:03:15 +0100

    :hammer: Show all candidates in category

--- client/src/components/AddCandidates.js
+++ client/src/components/AddCandidates.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
+import { SERVER_HOST } from "../config/global_constants";
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
 export default function AddCandidates() {
   const [candidateName, setCandidateName] = useState("");
   const [candidates, setCandidates] = useState([]);
+  const [cookies] = useCookies(["userId"]);
   const navigate = useNavigate();
   const location = useLocation();
   const category = location.state;
+
+  async function getCandidatesForCategory() {
+    const candidates = await axios.get(
+      `${SERVER_HOST}/candidates/${cookies.userId}/${category.name}`
+    );
+    setCandidates(candidates.data);
+  }
+
+  useEffect(() => {
+    getCandidatesForCategory();
+  });
 
   const handleCandidateNameChange = (e) => {
     setCandidateName(e.target.value);
@@ -16,18 +31,32 @@ export default function AddCandidates() {
   const addCandidate = (e) => {
     e.preventDefault();
 
-    const formData = new FormData();
+    let formData = new FormData();
     formData.append("candidateName", candidateName);
+    formData.append("categoryId", category.id);
+
+    axios
+      .post(`${SERVER_HOST}/insert_candidate`, formData, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => {})
+      .catch((error) => {
+        console.error("Error adding candidate:", error);
+      });
   };
 
   const doneAddCandidates = (e) => {
-    navigate("/");
+    navigate("/user_preferences", { state: category });
   };
 
   const addCandidateTags = (e) => {
     navigate("/add_candidate_tags", {
       state: {
         category: category,
+        candidate: {
+          name: e.target.innerHTML,
+          id: e.target.id,
+        },
       },
     });
   };
@@ -47,9 +76,13 @@ export default function AddCandidates() {
       </form>
       <button onClick={doneAddCandidates}>Done</button>
 
-      {candidates.map((candidate) => (
-        <div onClick={addCandidateTags}>{candidate.candidate_name}</div>
-      ))}
+      {candidates.length
+        ? candidates.map((candidate) => (
+            <div id={candidate.candidate_id} onClick={addCandidateTags}>
+              {candidate.candidate_name}
+            </div>
+          ))
+        : null}
     </div>
   );
 }
