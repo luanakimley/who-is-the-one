e4361f435eca37e5e3e802000e7208771cc1dc8c
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-27 00:50:36 +0100

    :hammer: Fix tag.js not returning a list of tags

--- server/db/Database.js
+++ server/db/Database.js
@@ -40,10 +40,18 @@ query(sql, values, callback)
     {
         console.log(err);
     }
-        if (Array.isArray(result) && result.length > 0)
+        if (Array.isArray(result))
         {
-           callback(result[0]);
+            if(result.length === 1)
+            {
+                callback(result[0]);
+            }
+            else
+            {
+                callback(result)
+            }
         }
+
         else
         {
           callback([]);
