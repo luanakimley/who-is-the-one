6587bcce49572c71108c8688e67b4a55587b14ca
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-26 18:10:09 +0100

    :sparkles: Add remove tag and remove tag for candidate routes

--- server/routes/tags.js
+++ server/routes/tags.js
@@ -28,8 +28,13 @@ router.get("/tags_by_category/:categoryId", (req, res) => {
         database.query(query, [categoryId], (result) => res.json(result));
       });
 
+router.delete("/remove_tag/:tagId", (req, res) => {
+  const tagId = req.params.tagId;
+  const query = "DELETE FROM tags WHERE tag_id = ?";
+  database.query(query, [tagId, candidateId], (result) => res.send(`Delete tag with ID ${tagId}`));
+});
 
-router.delete("/remove_tag/:tagId/:candidateId", (req, res) => {
+router.delete("/remove_tag_for_candidate/:tagId/:candidateId", (req, res) => {
   const tagId = req.params.tagId;
   const candidateId = req.params.candidateId;
   const query = "DELETE FROM tags_in_candidates WHERE tag_id = ? AND candidate_id = ?";
