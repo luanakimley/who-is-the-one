ee467ba8d00d8b917d2cf5f8b490808de3e584b2
luanakimley <luanaa.kimley@gmail.com>
2023-06-26 20:48:30 +0700

    :sparkles: Develop tag weight box component

---
+++ client/src/components/TagWeightBox.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function TagWeightBox() {
+  return (
+    <div>
+      <h2>Tag desc</h2>
+      <input readOnly type="range" value={60}></input>
+    </div>
+  );
+}
