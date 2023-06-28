d8ed18e317fd21231327cf77667b93bb13a78ab9
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-28 21:19:26 +0100

    :sparkles: Add edit user preference

--- server/routes/user_preferences.js
+++ server/routes/user_preferences.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const {
   getUserPreferencesByCategoryId,
   addUserPreference,
-  deleteUserPreference
+  deleteUserPreference,
+  editUserPreference
 } = require("../controllers/user_preferences");
 
 router
   .get("/user_preferences/:categoryId", getUserPreferencesByCategoryId)
   .post("/insert_user_preference", addUserPreference)
   .delete("/remove_user_preference/:categoryId/:tagId", deleteUserPreference)
+  .put("/edit_user_preference", editUserPreference)
 
 module.exports = router;
