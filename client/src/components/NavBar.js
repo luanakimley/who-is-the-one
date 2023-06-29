c5cd702db1d8cfc6d2f3a1996d59e21c53715a12
luanakimley <luanaa.kimley@gmail.com>
2023-06-29 23:59:05 +0800

    :lipstick: Underline active page in navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
 export default function NavBar() {
   const [cookies, setCookie, removeCookie] = useCookies(["userId"]);
+  const { pathname } = useLocation();
   const navigate = useNavigate();
 
   const handleLogOut = () => {
     removeCookie("userId");
     navigate("/login");
+  };
+
+  const test = () => {
+    console.log(pathname);
   };
 
   return (
@@ -25,28 +30,37 @@ export default function NavBar() {
           className="collapse navbar-collapse d-lg-flex"
           id="navbarsExample11"
         >
-          <ul className="navbar-nav col-lg-6 justify-content-lg-center">
-            <li className="nav-item">
-              <Link className="nav-link active" to="/">
+          <ul className="navbar-nav col-lg-6 justify-content-lg-center gap-4 align-items-center">
+            <li onClick={test} className="nav-item">
+              <Link className={pathname === "/" ? "" : "nav-link"} to="/">
                 <h3>Home</h3>
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" to="/categories">
+              <Link
+                className={pathname === "/categories" ? "" : "nav-link"}
+                to="/categories"
+              >
                 <h3>Categories</h3>
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" to="/tags">
+              <Link
+                className={pathname === "/tags" ? "" : "nav-link"}
+                to="/tags"
+              >
                 <h3>All Tags</h3>
               </Link>
             </li>
           </ul>
         </div>
 
-        <div className="d-lg-flex align-items-center">
+        <div className="d-lg-flex align-items-center gap-4">
           <li className="nav-item">
-            <Link className="nav-link active" to="/profile_page">
+            <Link
+              className={pathname === "/profile_page" ? "" : "nav-link"}
+              to="/profile_page"
+            >
               <h4>Profile</h4>
             </Link>
           </li>
