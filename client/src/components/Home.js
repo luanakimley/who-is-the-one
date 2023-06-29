60672aeac48541543724a7ed03d8f213256a5a53
luanakimley <luanaa.kimley@gmail.com>
2023-06-29 10:18:37 +0700

    :hammer: Start develop home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
+import logo from "../assets/logo_WITO_white.png";
 
 export default function Home() {
   const [cookies] = useCookies(["username"]);
@@ -12,9 +13,9 @@ export default function Home() {
       <NavBar />
 
       <div className="p-3 mb-2 bg-primary text-white">
-        <div className="container">
-          <h1>Home</h1>
-          <h2>Hello {cookies.username}!</h2>
+        <div className="top-margin container">
+          <img src={logo} alt="WITO Logo" width="400"></img>
+          <div className="card">Test</div>
         </div>
       </div>
     </div>
